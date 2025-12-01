cce21c01924f779028c62d30c8ad378d668fbb75
shiddarthbista <61994769+shiddarthbista@users.noreply.github.com>
2025-11-30 22:10:05 -0500

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,12 +7,12 @@ fun main() {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part1(testInput).println()
+    part2(testInput).println()
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 2)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
